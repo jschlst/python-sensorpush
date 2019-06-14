f383bc14dea3316118df738766af63d9ced8daa0
Jay Schulist <jayschulist@gmail.com>
2019-06-14 12:59:55 -0500

    lots of bug fixes and tracking of timeouts

--- README.md
+++ README.md
@@ -4,16 +4,24 @@ Python SensorPush API
 
 For use with [SensorPush](http://www.sensorpush.com) wireless sensors with alerts for monitoring temperature, humidity and more.
 
-Version 0.1.0
+Version 0.1.2
 
-Date 06/13/2019
+Date 06/14/2019
 
 Written by Jay Schulist <jayschulist@gmail.com>
+
 
 ## Example
 ```
 python sensorpush.py -u name@email.com -p password
 ```
+
+
+## Important
+[SensorPush API docs](http://www.sensorpush.com/api/docs)
+
+Authorization is required from support@sensorpush.com before API access is allowed.
+
 
 ## Acknowledgments
 - [SensorPush JavaScript API](https://github.com/malgorithms/sensorpush) written by Chris Coyne <ccoyne77@gmail.com>
--- sensorpush.py
+++ sensorpush.py
@@ -26,7 +26,7 @@ from __future__ import absolute_import
 
 import os, sys, re, getopt, getpass
 import time
-import datetime
+from datetime import datetime, timedelta
 import oauth2
 import urllib2
 import urlparse
@@ -36,9 +36,50 @@ import json
 class SensorPush(object):
     apiURL = "https://api.sensorpush.com"
 
+    # track authentication
+    authorization = None
+    accesstoken = None
+    lastAuthentication = None
+    lastToken = None
+    authTimeout = 60 * 60
+    tokenTimeout = 30 * 60
+
+    # track access
+    lastAccess = None
+    reqTimeout = 60     # max 1 req per minute
+
+    # track data
+    sensorData = None
+    gatewayData = None
+    sampleData = None
+
     def __init__(self, email, password):
         self.email = email
         self.password = password
+
+    def authOk(self):
+        if (self.lastAuthentication == None or self.lastToken == None):
+            return False
+        authTime = datetime.now() - self.lastAuthentication
+        tokenTime = datetime.now() - self.lastToken
+        if (authTime > timedelta(seconds=self.authTimeout)):
+            # need to re-autenticate
+            return False
+        if (tokenTime > timedelta(seconds=self.tokenTimeout)):
+            # need new token
+            return False
+        return True
+
+    def reqOk(self, block=True):
+        timeout = datetime.now() - self.lastAccess
+        if (timeout >= timedelta(seconds=self.reqTimeout)):
+            return True
+        if (block):
+            print("block request for %d seconds" % timeout.total_seconds())
+            time.sleep(timeout.total_seconds())
+            return True
+        else:
+            return False
 
     def authorize(self):
         path   = "/api/v1/oauth/authorize"
@@ -47,64 +88,90 @@ class SensorPush(object):
             'password': self.password
         }
         data = self.postToSensorPush(path, params)
-        print(data)
-        self.authorization = data
+        self.authorization  = data['authorization']
+        self.apikey         = data['apikey']
+        self.lastAuthentication = datetime.now()
+        print("authorization: %s" % self.authorization)
+        print("apikey: %s" % self.apikey)
+        print("last authentication: %s" % self.lastAuthentication)
         return self.authorization
 
     def accessToken(self):
         path = "/api/v1/oauth/accesstoken"
         params = {
-            authorization: self.authorization
+            'authorization': self.authorization
         }
         data = self.postToSensorPush(path, params)
-        print(data)
-        self.accesstoken = data
+        self.accesstoken = data['accesstoken']
+        self.lastToken = datetime.now()
+        print("accesstoken: %s" % self.accesstoken)
+        print("last token: %s" % self.lastToken)
         return self.accesstoken
+
+    def connect(self):
+        if (self.authOk()):
+            return True
+        print("connect to sensorpush API")
+        # do initial authorization or re-authorize
+        self.authorize()
+        self.accessToken()
+        return True
 
     def gateways(self):
         path = "/api/v1/devices/gateways"
-        params = {
-            accesstoken: self.accesstoken
-        }
-        data = self.postToSensorPush(path, params)
-        print(data)
+        self.connect()
+        self.reqOk()
+        data = self.postToSensorPush(path, params=None, accesstoken=self.accesstoken)
+        self.gatewayData = data
         return data
 
     def sensors(self):
         path = "/api/v1/devices/sensors"
-        params = {
-            accesstoken: self.accesstoken
-        }
-        data = self.postToSensorPush(path, params)
-        print(data)
+        self.connect()
+        self.reqOk()
+        data = self.postToSensorPush(path, params=None, accesstoken=self.accesstoken)
+        self.sensorData = data
         return data
 
-    def samples(self, limit, startTime):
+    def samples(self, limit=100, startTime=None, stopTime=None):
         path = "/api/v1/samples"
         params = {
-            accesstoken: self.accesstoken,
-            limit: limit,
-            startTime: startTime.isoformat()
+            'limit': limit
         }
-        data = self.postToSensorPush(path, params)
-        print(data)
+        if (startTime != None):
+            params['startTime'] = startTime.isoformat()
+        if (stopTime != None):
+            params['stopTime'] = stopTime.isoformat()
+        self.connect()
+        self.reqOk()
+        data = self.postToSensorPush(path, params, self.accesstoken)
+        self.sampleData = data
         return data
 
-    def postToSensorPush(self, path, params):
-        data = json.dumps(params, sort_keys=True)
+    def postToSensorPush(self, path, params=None, accesstoken=None):
         headers = {}
+        if (params != None):
+            data = json.dumps(params, sort_keys=True)
+            headers['Content-Length'] = len(data)
+            headers['Content-Type'] = 'application/json'
+        else:
+            data = ''
         headers['accept']       = 'application/json'
-        headers['Content-Type'] = 'application/json'
         headers['User-Agent']   = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-        headers['Content-Length'] = len(data)
+        if (accesstoken != None):
+            headers['Authorization'] = accesstoken
         req = urllib2.Request(self.apiURL + path, data, headers)
+        
         print(req.get_full_url())
         print(req.get_method())
         print(req.headers)
         print(req.get_data())
+
+        self.lastAccess = datetime.now()
         resp = urllib2.urlopen(req)
-        respData = resp.read()
-        print(respData)
+        print("last access time: %s" % str(self.lastAccess))
+        respData = json.loads(resp.read())
+        print(json.dumps(respData, sort_keys=True, indent=4))
         return respData
 
 
@@ -137,10 +204,8 @@ def main():
     sensor = SensorPush(email=username, password=password)
 
     # 1: get authorization code
-    sensor.authorize()
-
     # 2: get access token
-    sensor.accessToken()
+    # automatically checked and maintained through sensors(), gateways(), and samples() call
 
     # 3: get sensor data
     sensor.sensors()
@@ -148,9 +213,13 @@ def main():
     # 4: get gateway data
     sensor.gateways()
 
-    # 5: get temperature/humidity data from last 10 minutes
-    startTime = datetime.now() - 60 * 1000 * 10
-    sensor.samples(limit=10, startTime=startTime)
+    # 5: get temperature/humidity last 10 samples 
+    sensor.samples(limit=10)
+
+    # 6: get temperature/humidity last 10 minutes, max 1000 samples
+    stopTime = datetime.now()
+    startTime = stopTime - timedelta(minutes=10)
+    sensor.samples(limit=1000, startTime=startTime, stopTime=stopTime)
 
     sys.exit(0)
 
